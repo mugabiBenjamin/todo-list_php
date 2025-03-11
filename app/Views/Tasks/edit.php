6bccaf40da122907b4c81b4ff0850274c20192a2
mugabiBenjamin <mugabiben6@gmail.com>
2025-03-11 16:23:08 +0300

    Add class & remove label in edit.php file

--- app/Views/Tasks/edit.php
+++ app/Views/Tasks/edit.php
@@ -17,14 +17,13 @@ use App\Helpers\Security;
         <h2>Edit Task</h2>
         <form action="/update/<?php echo (int)$task['id']; ?>" method="POST">
             <input type="hidden" name="csrf_token" value="<?php echo Security::generateCsrfToken(); ?>">
-            <label for="name">Task Name:</label><br>
             <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($task['name']); ?>" required
                 maxlength="255" pattern="[A-Za-z0-9\s\-_.,!?]{3,255}"><br>
-            <small>Task name must be 3-255 characters.</small><br><br>
+            <small>Task name must be 3-50 characters.</small><br><br>
             <label for="completed">Completed:</label><br>
             <input type="checkbox" id="completed" name="completed"
                 <?php echo $task['completed'] ? 'checked' : ''; ?>><br><br>
-            <button type="submit">Update Task</button>
+            <button type="submit" class="btn-update">Update Task</button>
         </form>
         <a href="/">Back to Task List</a>
     </div>
