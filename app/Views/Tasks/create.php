b30a7b344be11700ae77f2bf8c1dcede9bb30073
mugabiBenjamin <mugabiben6@gmail.com>
2025-03-11 14:31:04 +0300

    Update create page file

--- app/Views/Tasks/create.php
+++ app/Views/Tasks/create.php
@@ -12,15 +12,15 @@ use App\Helpers\Security;
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 
-<body>
-    <div class="container">
+<body class="create-page">
+    <div class="container-md">
         <h2>Create New Task</h2>
         <form action="/tasks" method="POST">
             <input type="hidden" name="csrf_token" value="<?php echo Security::generateCsrfToken(); ?>">
-            <label for="name">Task Name:</label><br>
-            <input type="text" id="name" name="name" required maxlength="255" pattern="[A-Za-z0-9\s\-_.,!?]{3,255}"><br>
-            <small>Task name must be 3-255 characters.</small><br><br>
-            <button type="submit">Create Task</button>
+            <input type="text" id="name" name="name" required maxlength="50" pattern="[A-Za-z0-9\s\-_.,!?]{3,255}"
+                placeholder="Type your task here"><br>
+            <div><button type="submit">Create Task</button></div>
+            <small>Task name must be 3-50 characters.</small><br><br>
         </form>
         <a href="/">Back to Task List</a>
     </div>
