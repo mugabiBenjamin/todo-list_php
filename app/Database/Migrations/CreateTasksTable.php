c7c64ace27dda9f8bd3905680f4a958fc1f2153d
mugabiBenjamin <mugabiben6@gmail.com>
2026-07-17 18:06:15 +0300

    Refactor the CreateTaskTable file

--- app/Database/Migrations/CreateTasksTable.php
+++ app/Database/Migrations/CreateTasksTable.php
@@ -2,17 +2,20 @@
 
 namespace App\Database\Migrations;
 
-use App\Database\DatabaseManager;
+use App\Interfaces\DatabaseConnectionInterface;
 
 class CreateTasksTable
 {
-    public static function up(DatabaseManager $db)
+    public function __construct(private readonly DatabaseConnectionInterface $db) {}
+
+    public function up(): void
     {
         $sql = "CREATE TABLE IF NOT EXISTS tasks (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            completed TINYINT(1) DEFAULT 0
+            id      SERIAL PRIMARY KEY,
+            name    VARCHAR(255) NOT NULL,
+            completed BOOLEAN DEFAULT FALSE
         )";
-        $db->getConnection()->exec($sql);
+
+        $this->db->getConnection()->exec($sql);
     }
 }
