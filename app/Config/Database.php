93838cbfcc55753bbcb4c6713a02383cae64a2d8
mugabiBenjamin <mugabiben6@gmail.com>
2026-07-17 18:05:06 +0300

    Update the Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -2,14 +2,16 @@
 
 namespace App\Config;
 
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->safeLoad();
-
-return [
-    'host' => $_ENV['DB_HOST'],
-    'user' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD'],
-    'database' => $_ENV['DB_NAME'],
-];
+class Database
+{
+    public static function config(): array
+    {
+        return [
+            'host'     => $_ENV['DB_HOST'],
+            'port'     => $_ENV['DB_PORT'] ?? '5432',
+            'user'     => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'database' => $_ENV['DB_NAME'],
+        ];
+    }
+}
